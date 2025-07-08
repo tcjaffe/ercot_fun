027ee78c9910bc4aad4fbb76e22e098090f94589
Natasha Jaffe <tcjaffe@gmail.com>
2025-07-08 14:34:26 -0400

    Applied linter suggestions from pylint.

--- ercot_api_client.py
+++ ercot_api_client.py
@@ -7,7 +7,8 @@ USERNAME = os.getenv('ERCOT_USERNAME')
 PASSWORD = os.getenv('ERCOT_PASSWORD')
 
 # Authorization URL for signing into ERCOT Public API account
-AUTH_URL = "https://ercotb2c.b2clogin.com/ercotb2c.onmicrosoft.com/B2C_1_PUBAPI-ROPC-FLOW/oauth2/v2.0/token\
+AUTH_URL = "https://ercotb2c.b2clogin.com/ercotb2c.onmicrosoft.com/B2C_1_PUBAPI-ROPC-FLOW/\
+oauth2/v2.0/token\
 ?username={username}\
 &password={password}\
 &grant_type=password\
@@ -22,24 +23,30 @@ LMPS_NODES_ZONES_HUBS = "https://api.ercot.com/api/public-reports/np6-788-cd/lmp
 
 
 def _get_headers(access_token: str) -> dict[str,str]:
-    return {'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY, "Authorization": "Bearer " + access_token,}
+    return {
+        'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY, 
+        "Authorization": f"Bearer {access_token}",
+    }
 
 def _to_sced_format(dt: datetime) -> str:
     return dt.strftime('%Y-%m-%dT%H:%M:%S')
 
 def get_token() -> str:
+    """Returns a token for the current ERCOT session.  Lasts one hour."""
     # Sign In/Authenticate
-    auth_response = requests.post(AUTH_URL.format(username = USERNAME, password=PASSWORD))
+    auth_response = requests.post(
+        AUTH_URL.format(username = USERNAME, password=PASSWORD), 
+        timeout=30)
 
     # Retrieve access token
-    access_token = auth_response.json().get("access_token")
-    return access_token
+    return auth_response.json().get("access_token")
 
 def get_lmps_for_nodes_zones_and_hubs(
         access_token: str, 
         sced_from: datetime, 
         sced_to: datetime, 
         settlement_point: str) -> list:
+    """Returns a list of lmps for a given timestamp range and settlement_point, eg 'LZ_NORTH'"""
 
     response = requests.get(
         headers = _get_headers(access_token),
@@ -47,7 +54,8 @@ def get_lmps_for_nodes_zones_and_hubs(
             sced_from=_to_sced_format(sced_from), 
             sced_to=_to_sced_format(sced_to), 
             settlement_point=settlement_point
-        )
+        ),
+        timeout=30
     )
 
     if response.status_code != 200:
@@ -56,15 +64,12 @@ def get_lmps_for_nodes_zones_and_hubs(
     return response.json()['data']
 
 
-access_token = get_token()
-sced_from = datetime.strptime('2025/07/07 05:00:00', '%Y/%m/%d %H:%M:%S')
-sced_to = datetime.strptime('2025/07/07 05:30:00', '%Y/%m/%d %H:%M:%S')
 
 print('LMPs for load zone LZ_HOUSTON:')
 lz_houston_lmps = get_lmps_for_nodes_zones_and_hubs(
-    access_token=access_token,
-    sced_from=sced_from,
-    sced_to=sced_to,
+    access_token=get_token(),
+    sced_from=datetime.strptime('2025/07/07 05:00:00', '%Y/%m/%d %H:%M:%S'),
+    sced_to=datetime.strptime('2025/07/07 05:30:00', '%Y/%m/%d %H:%M:%S'),
     settlement_point='LZ_HOUSTON')
 
 for lmp in lz_houston_lmps:
